075ad7036bd412d49fa3b00505ac98130cd8642d
win10Device <unfatalbot@gmail.com>
2025-07-12 16:26:40 +1000

    Added SQL file for setting up database

---
+++ fetch.sql
@@ -0,0 +1,132 @@
+-- Fetch Bot database structure
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `fetch`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `banned_tags`
+--
+
+CREATE TABLE `banned_tags` (
+  `id` int(11) NOT NULL,
+  `tag` text NOT NULL,
+  `nsfw` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'Boolean to apply tag to filter on NSFW as well'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `characters`
+--
+
+CREATE TABLE `characters` (
+  `id` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `cat` int(11) NOT NULL COMMENT 'category id',
+  `fullname` text COMMENT 'Display name of character',
+  `fetchmeta` text NOT NULL COMMENT 'JSON string of metadata used for fetch',
+  `added` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `history`
+--
+
+CREATE TABLE `history` (
+  `id` varchar(32) NOT NULL,
+  `message` text COMMENT 'Discord Message ID',
+  `embed` text NOT NULL COMMENT 'embed data - JSON',
+  `raw_data` text NOT NULL COMMENT 'Raw fetch JSON',
+  `pending_review` tinyint(1) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `backward_post` varchar(32) DEFAULT NULL,
+  `guild` text COMMENT 'Discord Guild ID',
+  `channel` text COMMENT 'Discord Channel ID',
+  `cat` int(11) NOT NULL COMMENT 'Category ID',
+  `ch` int(11) NOT NULL COMMENT 'Character ID',
+  `value` text NOT NULL,
+  `forward_post` varchar(32) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `banned_tags`
+--
+ALTER TABLE `banned_tags`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `characters`
+--
+ALTER TABLE `characters`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `history`
+--
+ALTER TABLE `history`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `banned_tags`
+--
+ALTER TABLE `banned_tags`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `characters`
+--
+ALTER TABLE `characters`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
