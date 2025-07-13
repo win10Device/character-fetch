c592d6d938871700003e798a0aa2843f48b9de9e
win10Device <unfatalbot@gmail.com>
2025-07-13 12:01:23 +1000

    Fixed NaN result when comparing timestamps in page max scan

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Events, GatewayIntentBits, Collection, ActivityType } = require('discord.js');
 const config = require('./config.json');
-const { Collection } = require('discord.js')
-const { ActivityType } = require('discord.js')
 const axios = require('axios');
 const path = require('path');
 const fs = require('fs');
@@ -54,54 +52,59 @@ function scan() {
         stats.name_meta[ch.name] = ch.fullname;
         let meta = JSON.parse(ch.fetchmeta);
         let update = false;
-        const updated_at = Math.floor(new Date(ch.updated).getTime() / 1000);
-        if (time >= updated_at + 2629800) {
-          Object.keys(meta).forEach(async (key) => {
-            switch (key) {
-              case 'danbooru':
-                axios.get(`https://danbooru.donmai.us/posts?tags=${meta.danbooru.n}`).then(function (response) {
-                  let a = response.data.substring(response.data.lastIndexOf('<a class="paginator-page desktop-only"'));
-                  let c = parseInt(a.substring(a.indexOf('">')+2, a.indexOf('</a>')));
-                  console.log(`Getting Danbooru page max for ${ch.name}...`);
-                  if (isNaN(c)) {
-                    meta.danbooru.p = 1;
-                    console.log("HTML scrape returned null, assuming max page of 1...");
-                  } else {
-                    if (meta.danbooru.p != c) {
-                      update = true;
-                      meta.danbooru.p = (c >= 1000) ? 1000 : c;
+        let updated_at= Math.floor((new Date(ch.updated).getTime()) / 1000);
+        if (isNaN(updated_at)) updated_at = 1;
+        if (Math.abs(time - updated_at) >= 2629800) {
+          for (let i = 0; i <= 1; i++) {
+            Object.keys(meta).forEach(async (key) => {
+              switch (key) {
+                case 'danbooru':
+                  axios.get(`https://danbooru.donmai.us/posts?tags=${meta.danbooru.n}${(i==1)?"+rating%3Aexplicit":""}`).then(function (response) {
+                    let a = response.data.substring(response.data.lastIndexOf('<a class="paginator-page desktop-only"'));
+                    let c = parseInt(a.substring(a.indexOf('">')+2, a.indexOf('</a>')));
+                    console.log(`Getting Danbooru page max for ${ch.name}...`);
+                    if (isNaN(c)) {
+                      meta.danbooru.p[i] = 1;
+                      console.log("HTML scrape returned null, assuming max page of 1...");
+                    } else {
+                      if (meta.danbooru.p[i] != c) {
+                        update = true;
+                        meta.danbooru.p[i] = (c >= 1000) ? 1000 : c;
+                      }
                     }
+                  });
+                  break;
+                case 'gelbooru':
+                  axios.get(`https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&tags=${meta.gelbooru.n}+${(i==1)?"rating%3Aexplicit":"+rating%3ageneral"}&api_key=${config.gelbooru.key}&user_id=${config.gelbooru.usr}`).then(function (response) {
+                    console.log(`Getting Gelbooru page max for ${ch.name}...`);
+                    let c = Math.floor(response.data['@attributes'].count/100);
+                    if (meta.gelbooru.p[i] != c) {
+                      update = true;
+                      meta.gelbooru.p[i] = (c >= 200) ? 200 : c;
+                    }
+                  });
+                  break;
+                case 'pixiv':
+                  if (i == 0) {
+                    let char = encodeURIComponent(meta.pixiv.n);
+                    axios.get(`https://www.pixiv.net/ajax/search/artworks/${char}?word=${char}&order=date_d&mode=all&p=1&csw=0&s_mode=s_tag_full&type=all&lang=en&version=1c9a02aed9d76a9163650e70702997c6ac3bf7b5`,
+                      {
+                        headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:130.0) Gecko/20100101 Firefox/130.0', 'Cookie': config.pixiv.cookie }
+                      }
+                    ).then(function (response) {
+                      let c = response.data.body.illustManga.lastPage;
+                      console.log(`Getting Pixiv page max for ${ch.name}...`);
+                      if (meta.pixiv.p[0] != c) {
+                        update = true;
+                        meta.pixiv.p[0] = c;
+                      }
+                    });
                   }
-                });
-                break;
-              case 'gelbooru':
-                axios.get(`https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&tags=${meta.gelbooru.n}+rating%3ageneral&api_key=${config.gelbooru.key}&user_id=${config.gelbooru.usr}`).then(function (response) {
-                  console.log(`Getting Gelbooru page max for ${ch.name}...`);
-                  let c = Math.floor(response.data['@attributes'].count/100);
-                  if (meta.gelbooru.p != c) {
-                    update = true;
-                    meta.gelbooru.p = (c >= 200) ? 200 : c;
-                  }
-                });
-                break;
-              case 'pixiv':
-                let char = encodeURIComponent(meta.pixiv.n);
-                axios.get(`https://www.pixiv.net/ajax/search/artworks/${char}?word=${char}&order=date_d&mode=all&p=1&csw=0&s_mode=s_tag_full&type=all&lang=en&version=1c9a02aed9d76a9163650e70702997c6ac3bf7b5`,
-                  {
-                    headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:130.0) Gecko/20100101 Firefox/130.0', 'Cookie': config.pixiv.cookie }
-                  }
-                ).then(function (response) {
-                  let c = response.data.body.illustManga.lastPage;
-                  console.log(`Getting Pixiv page max for ${ch.name}...`);
-                  if (meta.pixiv.p != c) {
-                    update = true;
-                    meta.pixiv.p = c;
-                  }
-                });
-                break;
-            }
-          });
-          await sleep(1000);
+                  break;
+              }
+            });
+            await sleep(1000);
+          }
           if (update == true) {
             let str = JSON.stringify(meta);
             con.execute('UPDATE characters SET fetchmeta=?, updated=CURRENT_TIMESTAMP() WHERE id=?', [str,ch.id], (err, results) => {
@@ -116,6 +119,24 @@ function scan() {
       }
     } else {
       console.warn("No characters in database to check!");
+    }
+  });
+  con.query('SELECT * FROM banned_tags;', async function(err,results) {
+    if (err) {
+      throw err;
+      return;
+    }
+    if (results.length > 0) {
+      const banned_tags = { normal: [], explicit: [] };
+      results.forEach((result) => {
+        banned_tags.normal.push(result.tag);
+        if (result.nsfw) banned_tags.explicit.push(result.tag);
+      });
+      fs.writeFile('json/banned_tags.json', JSON.stringify(banned_tags,null,2), err => {
+        if (err) {
+          console.error(err);
+        }
+      });
     }
   });
 }
@@ -161,8 +182,8 @@ client.once(Events.ClientReady, async readyClient => {
     status: 'online'
   });
   client.lcommands = (await client.application.commands.fetch()).map((x)=>{return{id: x.id, name: x.name}})
-let userInstance = await client.users.cache;
-  console.log(userInstance);
+  //let userInstance = await client.users.cache;
+  //console.log(userInstance);
   /*client.lcommands.forEach((a) => {
     console.log(a);
   });*/
